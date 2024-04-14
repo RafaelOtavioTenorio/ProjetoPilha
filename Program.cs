cf6c5fdee1d090d29b50d94dd39d8c35627b85df
Rafael <r.otavio.tenorio@gmail.com>
2024-04-14 19:06:54 -0300

    InitialCommit

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿namespace ProjetoPilha
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("\nInício main");
+            TADPilha pilha = new TADPilha();
+            //pilha.Push(5);
+            //pilha.Push(11);
+            //pilha.Push(3);
+            //pilha.ImprimirPilha();
+            //pilha.Push(20);
+            //pilha.Push(5);
+            //pilha.ImprimirPilha();
+            //Console.WriteLine($"Elemento de código {pilha.Pop()} removido.");
+            //Console.WriteLine($"Elemento de código {pilha.Pop()} removido.");
+            //pilha.ImprimirPilha();
+
+            do
+            {
+                try
+                {
+                    Console.WriteLine("O que deseja fazer? \n1- Empilhar elemento. \n2- Desempilhar elemento. \n3- Imprimir pilha.");
+                    int selecao = int.Parse(Console.ReadLine());
+                    switch (selecao){
+                        case 1:
+                            Console.WriteLine("\nDigite o código do elemento a ser empilhado:");
+                            pilha.Push(int.Parse(Console.ReadLine()));
+                            break;
+                        case 2:
+                            pilha.Pop();
+                            Console.WriteLine("\nElemento removido.");
+                            break;
+                        case 3:
+                            pilha.ImprimirPilha();
+                            break;
+                    }
+                    Console.WriteLine("\nDeseja realizar outra operação? \n1- Sim. \n2- Não.");
+                    selecao = int.Parse(Console.ReadLine());
+                    if(selecao == 2)
+                    {
+                        break;
+                    }
+
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("\nErro:");
+                    Console.WriteLine(e.Message);
+                    Console.WriteLine("\n");
+                }
+            } while (true);
+            
+
+            Console.WriteLine("\nFim main");
+
+
+        }
+    }
+}
---
+++ TADPilha.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace ProjetoPilha
+{
+    internal class TADPilha
+    {
+        private class Nodulo
+        {
+            public int Codigo { get; set; }
+            public string? Descricao { get; set; }
+            public int Capacidade { get; set; }
+            public Nodulo Next { get; set; }
+
+            public Nodulo(int codigo, string descricao, int capacidade)
+            {
+                Codigo = codigo;
+                Descricao = descricao;
+                Capacidade = capacidade;
+                Next = null;
+            }
+        }
+
+        private Nodulo topo;
+
+        public TADPilha()
+        {
+            topo = null;
+        }
+        public int? Pop()
+        {
+            if(topo == null)
+            {
+                Console.WriteLine("A pilha está vazia.");
+                return null;
+            }
+            int codigo = topo.Codigo;
+            topo = topo.Next;
+            return codigo;
+        }
+        public void Push(int k)
+        {            
+            try
+            {
+                Console.WriteLine($"Digite a descrição do elemento de código {k}:");
+                string descricao = Console.ReadLine();
+                Console.WriteLine($"Digite a capacidade do elemento de código {k}");
+                int capacidade = int.Parse(Console.ReadLine());
+                Nodulo novoNodulo = new Nodulo(k, descricao, capacidade);
+                novoNodulo.Next = topo;
+                topo = novoNodulo;
+            } catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            } 
+        }
+        public void ImprimirPilha()
+        {
+            if (topo == null)
+            {
+                Console.WriteLine("A pilha está vazia.");
+                return;
+            }
+
+            Console.WriteLine("Conteúdo da pilha:");
+
+            Nodulo atual = topo;
+            while(atual != null)
+            {
+                Console.WriteLine($"Código: {atual.Codigo}, Descrição: {atual.Descricao}, Capacidade: {atual.Capacidade}.");
+                atual = atual.Next;
+            }
+        }
+    }
+}
